c2c2af7b969bde8539a841f26800c7e6591854be
Conor Gilsenan <conorgil@users.noreply.github.com>
2024-11-12 17:34:52 -0500

    Log today's date

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-alert("hello world!")
+const today = new Date();
+const monthName = today.toLocaleString('default', { month: 'long' });
+const dayNumber = today.getDate();
+const humanReadableDate = `${monthName} ${dayNumber}`;
+
+function main() {
+  console.log(humanReadableDate); // Output: November 4
+
+  // Get list of movie options from the Google Sheet somehow
+
+  // Pick one of the movies for today's date from the list at random
+
+  // Put the name of the selected movie into the text box on the webiste
+
+}
+
+main();
