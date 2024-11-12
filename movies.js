bcfd0ccd5f11f7ef032a3d54f127dafb4360712c
Conor Gilsenan <conorgil@users.noreply.github.com>
2024-11-12 17:30:44 -0500

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,23 @@
+movies = {
+  "November 4": [
+    "movie A",
+    "movie B",
+    "movie C"
+  ],
+  
+  "November 5": [
+    "V for Vendetta"
+  ],
+  
+  "November 6": [
+    "movie A",
+    "movie B",
+    "movie C"
+  ],
+  
+  "November 12": [
+    "movie 123",
+    "movie 456",
+    "movie XYZ"
+  ]
+}
