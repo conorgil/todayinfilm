c02bee327d62ae35d29bcd4b5754ccaafe3db556
kenziekiyomi <mackenzie.kiyomi.walker@gmail.com>
2024-11-14 22:15:14 -0500

    Update movies.mjs

--- movies.mjs
+++ movies.mjs
@@ -1,46 +1,88 @@
 export const movies = {
-  "January 1": [],
-  "January 2": [],
-  "January 3": [],
-  "January 4": [],
+  "January 1": [
+    "Rocky"
+  ],
+  "January 2": [
+    "No movie today go for a walk"
+  ],
+  "January 3": [
+    "Life and Death of Colonel Blimp"
+  ],
+  "January 4": [
+    "My Cousin Vinny"
+  ],
   "January 5": [],
-  "January 6": [],
+  "January 6": [
+    "Father of the Bride"
+  ],
   "January 7": [],
-  "January 8": [],
-  "January 9": [],
+  "January 8": [
+    "Gallipoli"
+  ],
+  "January 9": [
+    "Kramer vs. Kramer" 
+  ],
   "January 10": [],
   "January 11": [],
-  "January 12": [],
+  "January 12": [
+    "2001 A Space Odyssey"
+  ],
   "January 13": [],
-  "January 14": [],
+  "January 14": [
+    "Chicago"
+  ],
   "January 15": [],
   "January 16": [],
-  "January 17": [],
+  "January 17": [
+    "Au Revoir les Enfants"
+  ],
   "January 18": [],
-  "January 19": [],
+  "January 19": [
+    "Jane Eyre"
+  ],
   "January 20": [],
-  "January 21": [],
+  "January 21": [
+    "Rain Man"
+  ],
   "January 22": [],
   "January 23": [],
   "January 24": [],
-  "January 25": [],
-  "January 26": [],
+  "January 25": [
+    "Collateral"
+  ],
+  "January 26": [
+    "L.A. Confidential"
+  ],
   "January 27": [],
   "January 28": [],
   "January 29": [],
-  "January 30": [],
-  "January 31": [],
+  "January 30": [
+    "Bloody Sunday"
+  ],
+  "January 31": [
+    "Full Metal Jacket"
+  ],
   "February 1": [],
-  "February 2": [],
+  "February 2": [
+    "Groundhogs Day"
+  ],
   "February 3": [],
-  "February 4": [],
+  "February 4": [
+    "The Insider"
+  ],
   "February 5": [],
   "February 6": [],
   "February 7": [],
-  "February 8": [],
+  "February 8": [
+    "The Prestige"
+  ],
   "February 9": [],
-  "February 10": [],
-  "February 11": [],
+  "February 10": [
+    "Bring It On"
+  ],
+  "February 11": [
+    "Eternal Sunshine of the Spotless Mind"
+  ],
   "February 12": [],
   "February 13": [],
   "February 14": [],
@@ -48,157 +90,295 @@ export const movies = {
   "February 16": [],
   "February 17": [],
   "February 18": [],
-  "February 19": [],
+  "February 19": [
+    "The Matrix"
+  ],
   "February 20": [],
-  "February 21": [],
-  "February 22": [],
-  "February 23": [],
+  "February 21": [
+    "Malcom X"
+  ],
+  "February 22": [
+    "Miracle"
+  ],
+  "February 23": [
+    "The Umbrellas of Cherbourg"
+  ],
   "February 24": [],
-  "February 25": [],
+  "February 25": [
+    "Cool Runnings"
+  ],
   "February 26": [],
   "February 27": [],
-  "February 28": [],
+  "February 28": [
+    "Leap Year"
+  ],
   "February 29": [],
   "March 1": [],
-  "March 2": [],
+  "March 2": [
+    "Men in Black"
+  ],
   "March 3": [],
-  "March 4": [],
-  "March 5": [],
-  "March 6": [],
+  "March 4": [
+    "A Fish Called Wanda"
+  ],
+  "March 5": [
+    "Space Jam"
+  ],
+  "March 6": [
+    "The Death of Stalin"
+  ],
   "March 7": [],
-  "March 8": [],
-  "March 9": [],
+  "March 8": [
+    "Practical Magic"
+  ],
+  "March 9": [
+    "The Rock"
+  ],
   "March 10": [],
-  "March 11": [],
+  "March 11": [
+    "The Lives of Others"
+  ],
   "March 12": [],
-  "March 13": [],
+  "March 13": [
+    "Sound of Music"
+  ],
   "March 14": [],
-  "March 15": [],
-  "March 16": [],
-  "March 17": [],
+  "March 15": [
+    "Julius Caesar"
+  ],
+  "March 16": [
+    "Grave of the Fireflies"
+  ],
+  "March 17": [
+    "The Fugitive"
+  ],
   "March 18": [],
   "March 19": [],
   "March 20": [],
   "March 21": [],
   "March 22": [],
-  "March 23": [],
-  "March 24": [],
-  "March 25": [],
+  "March 23": [
+    "Singin' in the Rain"
+  ],
+  "March 24": [
+    "The Breakfast Club"
+  ],
+  "March 25": [
+    "The Great Escape"
+  ],
   "March 26": [],
-  "March 27": [],
+  "March 27": [
+    "Dial M for Murder"
+  ],
   "March 28": [],
   "March 29": [],
   "March 30": [],
   "March 31": [],
   "April 1": [],
-  "April 2": [],
-  "April 3": [],
+  "April 2": [
+    "Saving Mr. Banks"
+  ],
+  "April 3": [
+    "The Assassination of Jesse James by the Coward Robert Ford"
+  ],
   "April 4": [],
-  "April 5": [],
-  "April 6": [],
-  "April 7": [],
-  "April 8": [],
+  "April 5": [
+    "Le Samouri"
+  ],
+  "April 6": [
+    "Charlie Wilson's War"
+  ],
+  "April 7": [
+    "Avengers: Endgame"
+  ],
+  "April 8": [
+    "Captain Phillips"
+  ],
   "April 9": [],
   "April 10": [],
   "April 11": [],
-  "April 12": [],
-  "April 13": [],
-  "April 14": [],
+  "April 12": [
+    "BlacKKKlansman"
+  ],
+  "April 13": [
+    "Apollo 13"
+  ],
+  "April 14": [
+    "Titanic"
+  ],
   "April 15": [],
   "April 16": [],
   "April 17": [],
   "April 18": [],
   "April 19": [],
-  "April 20": [],
-  "April 21": [],
-  "April 22": [],
+  "April 20": [
+    "Clerks"
+  ],
+  "April 21": [
+    "Marie Antoinette"
+  ],
+  "April 22": [
+    "Minority Report"
+  ],
   "April 23": [],
   "April 24": [],
-  "April 25": [],
+  "April 25": [
+    "Miss Congeniality"
+  ],
   "April 26": [],
   "April 27": [],
-  "April 28": [],
-  "April 29": [],
-  "April 30": [],
-  "May 1": [],
-  "May 2": [],
+  "April 28": [
+    "Night of the Living Dead"
+  ],
+  "April 29": [
+    "The Godfather"
+  ],
+  "April 30": [
+    "Inglorious Basterds"
+  ],
+  "May 1": [
+    "Four Weddings and a Funeral"
+  ],
+  "May 2": [
+    "Zero Dark Thirty"
+  ],
   "May 3": [],
   "May 4": [],
-  "May 5": [],
+  "May 5": [
+    "The Right Stuff"
+  ],
   "May 6": [],
   "May 7": [],
-  "May 8": [],
+  "May 8": [
+    "Hot Fuzz"
+  ],
   "May 9": [],
   "May 10": [],
   "May 11": [],
-  "May 12": [],
+  "May 12": [
+    "The Terminator"
+  ],
   "May 13": [],
   "May 14": [],
   "May 15": [],
   "May 16": [],
   "May 17": [],
-  "May 18": [],
+  "May 18": [
+    "Turning Red"
+  ],
   "May 19": [],
-  "May 20": [],
+  "May 20": [
+    "My Fair Lady"
+  ],
   "May 21": [],
   "May 22": [],
-  "May 23": [],
+  "May 23": [
+    "Bonnie and Clyde"
+  ],
   "May 24": [],
-  "May 25": [],
+  "May 25": [
+    "Carrie"
+  ],
   "May 26": [],
   "May 27": [],
-  "May 28": [],
+  "May 28": [
+    "Dazed and Confused"
+  ],
   "May 29": [],
   "May 30": [],
   "May 31": [],
   "June 1": [],
-  "June 2": [],
+  "June 2": [
+    "Hairspray"
+  ],
   "June 3": [],
   "June 4": [],
-  "June 5": [],
-  "June 6": [],
-  "June 7": [],
-  "June 8": [],
-  "June 9": [],
+  "June 5": [
+    "Ferris Bueller's Day Off"
+  ],
+  "June 6": [
+    "Saving Private Ryan"
+  ],
+  "June 7": [
+    "Dogma"
+  ],
+  "June 8": [
+    "Taxi Driver"
+  ],
+  "June 9": [
+    "Clue"
+  ],
   "June 10": [],
-  "June 11": [],
-  "June 12": [],
+  "June 11": [
+    "Escape from Alcatraz"
+  ],
+  "June 12": [
+    "When Harry Met Sally"
+  ],
   "June 13": [],
   "June 14": [],
   "June 15": [],
-  "June 16": [],
-  "June 17": [],
+  "June 16": [
+    "Before Sunrise"
+  ],
+  "June 17": [
+    "All the President's Men"
+  ],
   "June 18": [],
   "June 19": [],
   "June 20": [],
-  "June 21": [],
+  "June 21": [
+    "Midsommar"
+  ],
   "June 22": [],
-  "June 23": [],
+  "June 23": [
+    "Silence of the Lambs"
+  ],
   "June 24": [],
   "June 25": [],
   "June 26": [],
   "June 27": [],
   "June 28": [],
-  "June 29": [],
+  "June 29": [
+    "Big Fish"
+  ],
   "June 30": [],
-  "July 1": [],
+  "July 1": [
+    "An Affair to Remember"
+  ],
   "July 2": [],
-  "July 3": [],
-  "July 4": [],
-  "July 5": [],
+  "July 3": [
+    "Jaws"
+  ],
+  "July 4": [
+    "Independence Day"
+  ],
+  "July 5": [
+    "Old Boy"
+  ],
   "July 6": [],
-  "July 7": [],
+  "July 7": [
+    "The Martian"
+  ],
   "July 8": [],
   "July 9": [],
   "July 10": [],
   "July 11": [],
   "July 12": [],
-  "July 13": [],
+  "July 13": [
+    "O Brother Where Art Thou"
+  ],
   "July 14": [],
   "July 15": [],
-  "July 16": [],
-  "July 17": [],
-  "July 18": [],
+  "July 16": [
+    "Oppenheimer"
+  ],
+  "July 17": [
+    "Mama Mia"
+  ],
+  "July 18": [
+    "Newsies"
+  ],
   "July 19": [],
   "July 20": [],
   "July 21": [],
@@ -206,87 +386,163 @@ export const movies = {
   "July 23": [],
   "July 24": [],
   "July 25": [],
-  "July 26": [],
+  "July 26": [
+    "Talented Mr Ripley"
+  ],
   "July 27": [],
   "July 28": [],
-  "July 29": [],
+  "July 29": [
+    "Top Gun"
+  ],
   "July 30": [],
-  "July 31": [],
-  "August 1": [],
-  "August 2": [],
+  "July 31": [
+    "Harry Potter and the Sorcerers Stone"
+  ],
+  "August 1": [
+    "The Hangover"
+  ],
+  "August 2": [
+    "Point Break"
+  ],
   "August 3": [],
-  "August 4": [],
-  "August 5": [],
-  "August 6": [],
+  "August 4": [
+    "Map of Tiny Perfect Things"
+  ],
+  "August 5": [
+    "Do the Right Thing"
+  ],
+  "August 6": [
+    "The Blues Brothers"
+  ],
   "August 7": [],
-  "August 8": [],
-  "August 9": [],
+  "August 8": [
+    "Rocky Horror Picture Show"
+  ],
+  "August 9": [
+    "What Ever Happened to Baby Jane"
+  ],
   "August 10": [],
   "August 11": [],
-  "August 12": [],
-  "August 13": [],
+  "August 12": [
+    "Inside Man"
+  ],
+  "August 13": [
+    "Moneyball"
+  ],
   "August 14": [],
-  "August 15": [],
+  "August 15": [
+    "Who Framed Roger Rabbit"
+  ],
   "August 16": [],
   "August 17": [],
-  "August 18": [],
+  "August 18": [
+    "Wet Hot American Summer"
+  ],
   "August 19": [],
   "August 20": [],
-  "August 21": [],
-  "August 22": [],
+  "August 21": [
+    "My Neighbor Totoro"
+  ],
+  "August 22": [
+    "Dog Day Afternoon"
+  ],
   "August 23": [],
   "August 24": [],
   "August 25": [],
-  "August 26": [],
-  "August 27": [],
+  "August 26": [
+    "The Day of the Jackal"
+  ],
+  "August 27": [
+    "The Great Muppet Caper"
+  ],
   "August 28": [],
-  "August 29": [],
+  "August 29": [
+    "Terminator 2 Judgment Day"
+  ],
   "August 30": [],
-  "August 31": [],
+  "August 31": [
+    "Amelie"
+  ],
   "September 1": [],
-  "September 2": [],
+  "September 2": [
+    "Gone with the Wind"
+  ],
   "September 3": [],
-  "September 4": [],
+  "September 4": [
+    "Stand By Me"
+  ],
   "September 5": [],
-  "September 6": [],
-  "September 7": [],
+  "September 6": [
+    "A Few Good Men"
+  ],
+  "September 7": [
+    "Heat"
+  ],
   "September 8": [],
   "September 9": [],
-  "September 10": [],
-  "September 11": [],
+  "September 10": [
+    "Saw"
+  ],
+  "September 11": [
+    "25th Hour"
+  ],
   "September 12": [],
-  "September 13": [],
+  "September 13": [
+    "In the Heat of the Night"
+  ],
   "September 14": [],
-  "September 15": [],
-  "September 16": [],
+  "September 15": [
+    "The Big Short"
+  ],
+  "September 16": [
+    "You've Got Mail"
+  ],
   "September 17": [],
   "September 18": [],
   "September 19": [],
   "September 20": [],
   "September 21": [],
   "September 22": [],
-  "September 23": [],
+  "September 23": [
+    "Air Force One"
+  ],
   "September 24": [],
   "September 25": [],
   "September 26": [],
   "September 27": [],
   "September 28": [],
   "September 29": [],
-  "September 30": [],
+  "September 30": [
+    "Fried Green Tomatoes"
+  ],
   "October 1": [],
-  "October 2": [],
-  "October 3": [],
-  "October 4": [],
+  "October 2": [
+    "Donnie Darko"
+  ],
+  "October 3": [
+    "Mean Girls"
+  ],
+  "October 4": [
+    "Your Name"
+  ],
   "October 5": [],
   "October 6": [],
   "October 7": [],
-  "October 8": [],
+  "October 8": [
+    "Ghostbusters"
+  ],
   "October 9": [],
   "October 10": [],
-  "October 11": [],
+  "October 11": [
+    "The Parent Trap"
+  ],
   "October 12": [],
-  "October 13": [],
-  "October 14": [],
+  "October 13": [
+    "50 First Dates"
+  ],
+  "October 14": [
+    "Witness for the Prosecution"
+  ],
   "October 15": [],
   "October 16": [],
   "October 17": [],
@@ -294,87 +550,159 @@ export const movies = {
   "October 19": [],
   "October 20": [],
   "October 21": [],
-  "October 22": [],
-  "October 23": [],
-  "October 24": [],
-  "October 25": [],
+  "October 22": [
+    "Top Gun Maverick"
+  ],
+  "October 23": [
+    "Memories of Murder"
+  ],
+  "October 24": [
+    "Fellowship of the Ring"
+  ],
+  "October 25": [
+    "Breakfast at Tiffany's"
+  ],
   "October 26": [],
   "October 27": [],
-  "October 28": [],
+  "October 28": [
+    "The Social Network"
+  ],
   "October 29": [],
-  "October 30": [],
-  "October 31": [],
-  "November 1": [],
-  "November 2": [],
+  "October 30": [
+    "Zodiac"
+  ],
+  "October 31": [
+    "Halloween"
+  ],
+  "November 1": [
+    "Coco"
+  ],
+  "November 2": [
+    "E.T."
+  ],
   "November 3": [],
-  "November 4": [],
+  "November 4": [
+    "Argo"
+  ],
   "November 5": [
     "V for Vendetta"
   ],
-  "November 6": [],
+  "November 6": [
+    "How to Lose a Guy in 10 Days"
+  ],
   "November 7": [],
-  "November 8": [],
-  "November 9": [],
+  "November 8": [
+    "Knives Out"
+  ],
+  "November 9": [
+    "Atomic Blonde"
+  ],
   "November 10": [],
   "November 11": [],
-  "November 12": [],
+  "November 12": [
+    "Back to the Future"
+  ],
   "November 13": [
-    "movie 123",
-    "movie 456",
-    "movie XYZ",
-    "We did it!",
-    "The Incredibles because it is the date of Elastigirl's retirement."
+    "The Incredibles"
   ],
   "November 14": [
-    "MKW movie 1",
-    "Hello Hello Movie 2",
-    "THIRD MOVIE TEST"
+    "National Treasure"
   ],
-  "November 15": [],
-  "November 16": [],
-  "November 17": [],
+  "November 15": [
+    "In Cold Blood"
+  ],
+  "November 16": [
+    "Children of Men"
+  ],
+  "November 17": [
+    "The American President"
+  ],
   "November 18": [],
   "November 19": [],
   "November 20": [],
   "November 21": [],
-  "November 22": [],
+  "November 22": [
+    "J.F.K."
+  ],
   "November 23": [],
   "November 24": [],
   "November 25": [],
-  "November 26": [],
+  "November 26": [
+    "Pride & Prejudice"
+  ],
   "November 27": [],
-  "November 28": [],
-  "November 29": [],
+  "November 28": [
+    "Prisoners"
+  ],
+  "November 29": [
+    "Raising Arizona"
+  ],
   "November 30": [],
   "December 1": [],
-  "December 2": [],
-  "December 3": [],
-  "December 4": [],
-  "December 5": [],
+  "December 2": [
+    "Casablanca"
+  ],
+  "December 3": [
+    "Hunt for Red October"
+  ],
+  "December 4": [
+    "Judas and the Black Messiah"
+  ],
+  "December 5": [
+    "Close Encounters of the Third Kind"
+  ],
   "December 6": [],
   "December 7": [],
   "December 8": [],
   "December 9": [],
   "December 10": [],
-  "December 11": [],
+  "December 11": [
+    "Goodfellas"
+  ],
   "December 12": [],
-  "December 13": [],
-  "December 14": [],
+  "December 13": [
+    "Twelve Monkeys"
+  ],
+  "December 14": [
+    "The Truman Show"
+  ],
   "December 15": [],
-  "December 16": [],
+  "December 16": [
+    "Man on Fire"
+  ],
   "December 17": [],
-  "December 18": [],
-  "December 19": [],
+  "December 18": [
+    "The Holiday"
+  ],
+  "December 19": [
+    "Elf"
+  ],
   "December 20": [],
-  "December 21": [],
-  "December 22": [],
-  "December 23": [],
-  "December 24": [],
-  "December 25": [],
+  "December 21": [
+    "Sunset Boulevard"
+  ],
+  "December 22": [
+    "Home Alone"
+  ],
+  "December 23": [
+    "Scrooged"
+  ],
+  "December 24": [
+    "Die Hard"
+  ],
+  "December 25": [
+    "Muppet Christmas Carol"
+  ],
   "December 26": [],
-  "December 27": [],
+  "December 27": [
+    "The Drop"
+  ],
   "December 28": [],
   "December 29": [],
-  "December 30": [],
-  "December 31": []
+  "December 30": [
+    "The Thin Man"
+  ],
+  "December 31": [
+    "200 Cigarettes"
+  ]
 };
